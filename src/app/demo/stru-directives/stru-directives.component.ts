b873cd46ea3aa5b1934d7cd5ce58876b92f929b0
popzhou <izhoujialiang@qq.com>
2017-12-13 19:53:36 +0800

    directives

--- src/app/demo/stru-directives/stru-directives.component.ts
+++ src/app/demo/stru-directives/stru-directives.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, AfterViewInit, ViewChild, AfterContentInit, AfterContentChecked } from '@angular/core';
 import _ from 'lodash';
+import * as moment from 'moment';
 import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 import { SimpleUser, SimpleUserData } from '../../models/simple-user';
+import { UserlistTrackbyComponent } from './userlist-trackby/userlist-trackby.component';
 
 @Component({
   selector: 'app-stru-directives',
   templateUrl: './stru-directives.component.html',
   styleUrls: ['./stru-directives.component.css']
 })
-export class StruDirectivesComponent implements OnInit {
+export class StruDirectivesComponent implements OnInit, AfterViewChecked, AfterViewInit,AfterContentInit, AfterContentChecked {
   currentUser;
   htmlCodeGuardAgainst: SafeHtml;
   users: SimpleUser[];
   tempString: string;
+  @ViewChild(UserlistTrackbyComponent) viewChild: UserlistTrackbyComponent;
 
   constructor(private domSanitizer: DomSanitizer) { }
 
@@ -23,6 +26,23 @@ export class StruDirectivesComponent implements OnInit {
 
     this.getTrustHtmlCodeGuardAgainst();
     this.users = SimpleUserData;
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+    console.log(this.viewChild.users);
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
   }
 
   getTrustHtmlCodeGuardAgainst() {
@@ -38,5 +58,9 @@ export class StruDirectivesComponent implements OnInit {
     var randomName = 'random' + Math.floor(Math.random() * 100);
     var user = { id: 3, name: randomName } as SimpleUser;
     this.users.push(user);
+  }
+
+  removeUser() {
+    this.users.pop();
   }
 }
--- src/app/demo/stru-directives/userlist-trackby/userlist-trackby.component.ts
+++ src/app/demo/stru-directives/userlist-trackby/userlist-trackby.component.ts
@@ -1,35 +1,61 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy, DoCheck } from '@angular/core';
 import { SimpleUser } from '../../../models/simple-user';
+import * as moment from 'moment';
+import _ from 'lodash';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-userlist-trackby',
   templateUrl: './userlist-trackby.component.html',
   styleUrls: ['./userlist-trackby.component.css']
 })
-export class UserlistTrackbyComponent implements OnInit, OnChanges, OnDestroy {
+export class UserlistTrackbyComponent implements OnInit, OnChanges, OnDestroy, DoCheck {
   @Input() users: SimpleUser[];
   @Input() tempString: string;
-  changeLog: Array<string> = [];
+  changeLog: string[] = [];
+  oldUsers: SimpleUser[];
+  usersChangeTimes: number = 0;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('ngOnInit' + Date.now().toString());
+    console.log('ngOnInit  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+
+    this.oldUsers = _.clone(this.users);
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges');
+    console.log('ngOnChanges  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+
     for (let propName in changes) {
       let chng = changes[propName];
       let cur = JSON.stringify(chng.currentValue);
       let prev = JSON.stringify(chng.previousValue);
       this.changeLog.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
     }
-    console.log(this.changeLog);
+    // console.log(this.changeLog);
   }
 
   ngOnDestroy() {
-    console.log('OnDestroy');
+    console.log('OnDestroy  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck  ' + moment(Date.now()).format('YYYY-MM-DD HH:mm:ss'));
+
+    let usersChanged = false;
+
+    let result = _.isEqual(this.users, this.oldUsers);
+    if (!result) {
+      console.log('users' + JSON.stringify(this.users));
+      console.log('oldUsers' + JSON.stringify(this.oldUsers));
+      usersChanged = true;
+      this.usersChangeTimes += 1;
+      this.oldUsers = _.clone(this.users);
+
+      let msg = `users changes  ${this.usersChangeTimes} times.`;
+      this.changeLog.push(msg);
+    }
   }
 
   trackByFn(index: number, item) {
